9f2f23358951a2bbd6e2a662017758fc070dd3d3
Thanh Long Nguyen <long20002000ht@gmail.com>
2021-06-22 00:22:12 +0700

    change pagination.js file

--- js/pagination.js
+++ js/pagination.js
@@ -2,38 +2,46 @@ $(document).ready(function(){
     var pShow = 12;
     var pTotal = $('.gategory-product .gategory-product-list').length;
     var numPages = pTotal/pShow;
-    $('.pagination-bar').append('<li class="prev"><a href="#" ><i class="fa fa-chevron-left"></i>Previous</a></li>');
+    var lastPage = Math.ceil(numPages)-1;
+    $('.pagination-bar').append('<li><span><a href="#" class="prev" ><i class="fa fa-chevron-left"></i>Previous</a></span></li>');
     for(i = 0;i < numPages;i++) {
         var pageNum = i + 1;
-        $('.pagination-bar').append('<li class="active"><span><a class="pagi-num" href="#" rel="'+i+'">'+pageNum+'</a></span></li> ');
+        $('.pagination-bar').append('<li><span><a class="pagi-num" href="#" rel="'+i+'">'+pageNum+'</a></span></li> ');
     }
-    $('.pagination-bar').append('<li class="next"><a href="#" >Next<i class="fa fa-chevron-right"></i></a></li>');
+    $('.pagination-bar').append('<li><span><a href="#" class="next">Next<i class="fa fa-chevron-right"></i></a></span></li>');
     $('.gategory-product .gategory-product-list').hide();
     $('.gategory-product .gategory-product-list').slice(0, pShow).show();
     $('.pagination-bar .pagi-num:first').addClass('active');
-    console.log($('.pagination-bar .pagi-num').text());
-    
+    $('.prev').addClass('disabled');
     $('.pagination-bar .pagi-num').bind('click', function(){
         $('.pagination-bar .pagi-num').removeClass('active');
         $(this).addClass('active');
         var currPage = $(this).attr('rel');
         var startItem = currPage * pShow;
         var endItem = startItem + pShow;
-        $('.gategory-product .gategory-product-list').css('opacity','0.0').hide()
-        .slice(startItem, endItem).css('display','table-row').animate({opacity:1}, 300);
+        $('.gategory-product .gategory-product-list').css('opacity','0.0').hide().
+        slice(startItem, endItem).css('display','table-row').animate({opacity:1}, 300);
+
+        if($(this).attr('rel')==0){
+            $('.pagination-bar .prev').addClass('disabled');
+            $('.pagination-bar .prev').removeClass('active');
+            $('.pagination-bar .next').addClass('active');
+            $('.pagination-bar .next').removeClass('disabled');  
+        }
+        else if($(this).attr('rel')==lastPage){
+            $('.pagination-bar .prev').addClass('active');
+            $('.pagination-bar .prev').removeClass('disabled');
+            $('.pagination-bar .next').addClass('disabled');
+            $('.pagination-bar .next').removeClass('active');
+        }
+        else{
+            $('.pagination-bar .prev').addClass('active');
+            $('.pagination-bar .prev').removeClass('disabled');
+            $('.pagination-bar .next').addClass('active');    
+            $('.pagination-bar .next').removeClass('disabled');    
+        }
     });
-    if($('.pagination-bar .pagi-num').text()==1){
-        $('.prev').addClass('disabled');
-        $('.next').addClass('active');
-    }
-    else if($('.pagination-bar .pagi-num').text()==Math.ceil(numPages)){
-        $('.prev').addClass('active');
-        $('.next').addClass('disabled');
-    }
-    else{
-        $('.prev').addClass('active');
-        $('.next').addClass('active');
-    }
+    
     
 
     
